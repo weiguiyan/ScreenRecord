664c18c2d4beede785b4a23070251d320696f99d
weiguiyan <46864974+weiguiyan@users.noreply.github.com>
2022-06-08 12:33:47 +0800

    First upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Screen from './Screen/Screen';
+function App() {
+  console.log("App_Component")
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      <Screen/>
+    </div>
+  );
+}
+
+export default App;
